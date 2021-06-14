01164b05f2caf952f09670aba6221c13d3a916d9
Guilherme Vinicius <guilhermeferreira2529@hotmail.com>
2021-06-14 18:27:34 -0300

    Erro de digitação no script corrigido

--- Cenário/Fase 1.gd
+++ Cenário/Fase 1.gd
@@ -10,7 +10,8 @@ func _process(_delta):
 		$ColorRect/AnimationPlayer.play("FadeOut")
 		yield(get_node("ColorRect/AnimationPlayer"), "animation_finished")
 		get_tree().paused = false
-		get_tree().change_scene("res:/WinGame.tscn")
+		get_tree().change_scene("res://WinGame.tscn")
+		print("a")
 	
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
